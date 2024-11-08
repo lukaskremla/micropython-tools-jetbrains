ccc7c49acf99f515f207e0af4ef5f69d00a1fe50
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2024-11-09 00:38:11 +0200

    Better settings UI

--- src/main/kotlin/com/jetbrains/micropython/settings/MicroPythonSettingsPanel.kt
+++ src/main/kotlin/com/jetbrains/micropython/settings/MicroPythonSettingsPanel.kt
@@ -41,112 +41,116 @@ import javax.swing.JPanel
  * @author vlan
  */
 class MicroPythonSettingsPanel(private val module: Module, disposable: Disposable) : JPanel(BorderLayout()) {
-  private val deviceTypeCombo = ComboBox(MicroPythonDeviceProvider.providers.toTypedArray())
+    private val deviceTypeCombo = ComboBox(MicroPythonDeviceProvider.providers.toTypedArray())
 
-  var deviceProviderUrl = ""
-  private var docsHyperlink = ActionLink("") { BrowserUtil.browse(deviceProviderUrl) }
+    var deviceProviderUrl = ""
+    private var docsHyperlink = ActionLink("") { BrowserUtil.browse(deviceProviderUrl) }
 
-  private val parameters = module.microPythonFacet?.let { facet ->
-  ConnectionParameters(
-    uart = facet.configuration.uart,
-    url = facet.configuration.webReplUrl,
-    password = "",
-    portName = facet.configuration.portName,
-  )} ?: ConnectionParameters("COM1")//todo reasonable port name
+    private val parameters = module.microPythonFacet?.let { facet ->
+        ConnectionParameters(
+            uart = facet.configuration.uart,
+            url = facet.configuration.webReplUrl,
+            password = "",
+            portName = facet.configuration.portName,
+        )
+    } ?: ConnectionParameters("COM1")//todo reasonable port name
 
-  val connectionPanel:DialogPanel
+    val connectionPanel: DialogPanel
 
-  init {
-    border = IdeBorderFactory.createEmptyBorder(UIUtil.PANEL_SMALL_INSETS)
-    runWithModalProgressBlocking(module.project, "Save password") {
-      parameters.password =
-        module.project.service<ConnectCredentials>().retrievePassword(parameters.url)
+    init {
+        border = IdeBorderFactory.createEmptyBorder(UIUtil.PANEL_SMALL_INSETS)
+        runWithModalProgressBlocking(module.project, "Save password") {
+            parameters.password = module.project.service<ConnectCredentials>().retrievePassword(parameters.url)
+        }
+        val deviceContentPanel = FormBuilder.createFormBuilder().addLabeledComponent("Device type:", deviceTypeCombo)
+            .addComponent(docsHyperlink).panel
+
+        connectionPanel = panel {
+            group("Connection") {
+                buttonsGroup {
+                    row {
+                        radioButton("Serial").bindSelected(parameters::uart)
+                        textField().bindText(parameters::portName).label("Port")
+                    }
+                    separator()
+                    row {
+                        radioButton("WebREPL").bindSelected({ !parameters.uart }, { parameters.uart = !it })
+                    }
+                    indent {
+                        row {
+                            textField().bindText(parameters::url).label("URL: ").columns(40)
+                                .validationInfo { field ->
+                                    val msg = messageForBrokenUrl(field.text)
+                                    msg?.let { error(it).withOKEnabled() }
+                                }
+                        }.layout(RowLayout.LABEL_ALIGNED)
+                        row {
+                            passwordField().bindText(parameters::password).label("Password: ")
+                                .comment("(4..9 symbols)")
+                                .columns(40)
+                                .validationInfo { field ->
+                                    if (field.password.size !in PASSWORD_LENGHT) error("Allowed password length is $PASSWORD_LENGHT").withOKEnabled() else null
+                                }
+                        }.layout(RowLayout.LABEL_ALIGNED)
+                    }
+                }
+            }.topGap(TopGap.MEDIUM)
+
+        }.apply {
+            registerValidators(disposable)
+            validateAll()
+
+        }
+        add(deviceContentPanel, BorderLayout.NORTH)
+        add(connectionPanel, BorderLayout.CENTER)
+
+        deviceTypeCombo.apply {
+            renderer = object : SimpleListCellRenderer<MicroPythonDeviceProvider>() {
+                override fun customize(
+                    list: JList<out MicroPythonDeviceProvider>,
+                    value: MicroPythonDeviceProvider?,
+                    index: Int,
+                    selected: Boolean,
+                    hasFocus: Boolean
+                ) {
+                    text = value?.presentableName ?: return
+                }
+            }
+            addActionListener {
+                docsHyperlink.apply {
+                    deviceProviderUrl = selectedProvider.documentationURL
+                    text = "Learn more about setting up ${selectedProvider.presentableName} devices"
+                    repaint()
+                }
+            }
+        }
     }
-    val deviceContentPanel = FormBuilder.createFormBuilder()
-      .addLabeledComponent("Device type:", deviceTypeCombo)
-      .addComponent(docsHyperlink)
-      .panel
 
-    connectionPanel = panel {
-      buttonsGroup("Connection") {
-        row {
-          radioButton("Serial")
-            .bindSelected(parameters::uart)
-        }
-        row {
-          textField()
-            .label("Port")
-            .bindText(parameters::portName)
-        }
-        row {
-          radioButton("WebREPL").bindSelected({ !parameters.uart }, { parameters.uart = !it })
-        }
-        row {
-          textField()
-            .bindText(parameters::url)
-          .label("URL: ").columns(40)
-          .validationInfo { field ->
-            val msg = messageForBrokenUrl(field.text)
-            msg?.let { error(it).withOKEnabled() }
-          }
-      }.layout(RowLayout.LABEL_ALIGNED)
-      row {
-        passwordField()
-          .bindText(parameters::password)
-          .label("Password (4..9 symbols): ").columns(40)
-          .validationInfo { field ->
-            if (field.password.size !in PASSWORD_LENGHT) error("Allowed password length is $PASSWORD_LENGHT").withOKEnabled() else null
-          }
-      }.layout(RowLayout.LABEL_ALIGNED)
-      }
-
-    }.apply {
-      registerValidators(disposable)
-      validateAll()
+    fun isModified(configuration: MicroPythonFacetConfiguration): Boolean {
+        return deviceTypeCombo.selectedItem != configuration.deviceProvider || connectionPanel.isModified()
     }
-    add(deviceContentPanel, BorderLayout.NORTH)
-    add(connectionPanel, BorderLayout.CENTER)
 
-    deviceTypeCombo.apply {
-      renderer = object: SimpleListCellRenderer<MicroPythonDeviceProvider>() {
-        override fun customize(list: JList<out MicroPythonDeviceProvider>, value: MicroPythonDeviceProvider?,
-                               index: Int, selected: Boolean, hasFocus: Boolean) {
-          text = value?.presentableName ?: return
+    fun getDisplayName(): String = "MicroPython"
+
+    fun apply(configuration: MicroPythonFacetConfiguration, facet: MicroPythonFacet) {
+        configuration.deviceProvider = selectedProvider
+        connectionPanel.apply()
+        configuration.webReplUrl = parameters.url
+        configuration.uart = parameters.uart
+        configuration.portName = parameters.portName
+        runWithModalProgressBlocking(facet.module.project, "Save password") {
+            facet.module.project.service<ConnectCredentials>()
+                .savePassword(configuration.webReplUrl, parameters.password)
         }
-      }
-      addActionListener {
-        docsHyperlink.apply {
-          deviceProviderUrl = selectedProvider.documentationURL
-          text = "Learn more about setting up ${selectedProvider.presentableName} devices"
-          repaint()
-        }
-      }
     }
-  }
 
-  fun isModified(configuration: MicroPythonFacetConfiguration, facet: MicroPythonFacet): Boolean {
-    return deviceTypeCombo.selectedItem != configuration.deviceProvider || connectionPanel.isModified()
-  }
-
-  fun getDisplayName(): String = "MicroPython"
-
-  fun apply(configuration: MicroPythonFacetConfiguration, facet: MicroPythonFacet) {
-    configuration.deviceProvider = selectedProvider
-    connectionPanel.apply()
-    configuration.webReplUrl = parameters.url
-    configuration.uart = parameters.uart
-    configuration.portName = parameters.portName
-    runWithModalProgressBlocking(facet.module.project, "Save password") {
-      facet.module.project.service<ConnectCredentials>().savePassword(configuration.webReplUrl, parameters.password)
+    fun reset(configuration: MicroPythonFacetConfiguration) {
+        deviceTypeCombo.selectedItem = configuration.deviceProvider
+        connectionPanel.reset()
     }
-  }
 
-  fun reset(configuration: MicroPythonFacetConfiguration, facet: MicroPythonFacet) {
-    deviceTypeCombo.selectedItem = configuration.deviceProvider
-    connectionPanel.reset()
-  }
-
-  private val selectedProvider: MicroPythonDeviceProvider
-    get() = deviceTypeCombo.selectedItem as MicroPythonDeviceProvider
+    private val selectedProvider: MicroPythonDeviceProvider
+        get() = deviceTypeCombo.selectedItem as MicroPythonDeviceProvider
 }
+
 private val PASSWORD_LENGHT = 4..9
