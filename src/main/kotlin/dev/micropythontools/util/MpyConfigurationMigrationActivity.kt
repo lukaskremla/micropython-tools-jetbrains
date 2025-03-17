6aabf1bbce4ad7e6644e8a5deea2535f103fc27b
Lukáš Kremla <155779787+lukaskremla@users.noreply.github.com>
2025-03-17 11:32:12 +0100

    Prepared logic for cleaning up the old stub libraries

--- src/main/kotlin/dev/micropythontools/util/MpyConfigurationMigrationActivity.kt
+++ src/main/kotlin/dev/micropythontools/util/MpyConfigurationMigrationActivity.kt
@@ -42,88 +42,129 @@ class MpyConfigurationMigrationActivity : ProjectActivity, DumbAware {
     override suspend fun execute(project: Project) {
         val settings = project.service<MpySettingsService>()
 
-        delay(2)
+        delay(2000)
 
         // Avoid running migration code if the settings version is up to date
-        if (settings.state.settingsVersion == 2) {
-            return
-        }
+        if (true) {//if (settings.state.settingsVersion != 2) {
+            val runManagerImpls = RunManagerImpl.getInstanceImpl(project)
 
-        val runManagerImpls = RunManagerImpl.getInstanceImpl(project)
+            var hasModifiedSomething = false
 
-        var hasModifiedSomething = false
+            runManagerImpls.allConfigurationsList.forEach { configuration ->
+                // Create a temporary element to which the configuration attributes are dumped and write it
+                val element = org.jdom.Element("temporaryConfig")
+                configuration.writeExternal(element)
 
-        runManagerImpls.allConfigurationsList.forEach { configuration ->
-            // Create a temporary element to which the configuration attributes are dumped and write it
-            val element = org.jdom.Element("temporaryConfig")
-            configuration.writeExternal(element)
+                // Now the attributes can be checked
+                // They're of no interest and can be skipped if they aren't of the old "micropython-tools-flash-configuration"
+                if (!element.attributes.toString().contains("micropython-tools-flash-configuration")) return@forEach
 
-            // Now the attributes can be checked
-            // They're of no interest and can be skipped if they aren't of the old "micropython-tools-flash-configuration"
-            if (!element.attributes.toString().contains("micropython-tools-flash-configuration")) return@forEach
+                element.attributes.forEach { attribute ->
+                    if (attribute.value == "micropython-tools-flash-configuration") {
+                        // Update the configuration ID
+                        attribute.value = "micropython-tools-upload-configuration"
+                    }
 
-            element.attributes.forEach { attribute ->
-                if (attribute.value == "micropython-tools-flash-configuration") {
-                    // Update the configuration ID
-                    attribute.value = "micropython-tools-upload-configuration"
+                    if (attribute.value.contains("Flash")) {
+                        attribute.value = attribute.value.replace("Flash", "Upload")
+                    }
                 }
 
-                if (attribute.value.contains("Flash")) {
-                    attribute.value = attribute.value.replace("Flash", "Upload")
+                // Handle switchToReplOnSuccess option
+                element.children.forEach { child ->
+                    if (child.name == "option") {
+                        val attribute = child.getAttribute("name")
+
+                        if (attribute.value == "runReplOnSuccess") {
+                            attribute.value = "switchToReplOnSuccess"
+                        }
+                    }
                 }
+
+                configuration.readExternal(element)
+
+                hasModifiedSomething = true
             }
 
-            // Handle switchToReplOnSuccess option
-            element.children.forEach { child ->
-                if (child.name == "option") {
-                    val attribute = child.getAttribute("name")
+            // The migration process has completed fully, the settings version can be saved
+            settings.state.settingsVersion = 2
 
-                    if (attribute.value == "runReplOnSuccess") {
-                        attribute.value = "switchToReplOnSuccess"
+            if (hasModifiedSomething) {
+                Notifications.Bus.notify(
+                    Notification(
+                        NOTIFICATION_GROUP,
+                        "MicroPython tools update",
+                        "MicroPython Tools run configurations have been automatically updated.<br><br>" +
+                                "For the changes to take effect, the IDE needs to be restarted.<br><br>" +
+                                "NOTE: If your run configurations were stored in a file, you'll need to reconfigure them after restart.",
+                        NotificationType.WARNING
+                    ).addAction(object : NotificationAction("Restart IDE") {
+                        override fun actionPerformed(e: AnActionEvent, notification: Notification) {
+                            ProjectManager.getInstance().reloadProject(project)
+                            notification.expire()
+                        }
+                    }),
+                    project
+                )
+
+                withContext(Dispatchers.EDT) {
+                    val clickResult = MessageDialogBuilder.Message(
+                        "MicroPython Tools Update",
+                        "MicroPython Tools run configurations have been automatically updated.\n\n" +
+                                "For the changes to take effect, the IDE needs to be restarted.\n\n" +
+                                "NOTE: If your run configurations were stored in a file, you'll need to reconfigure them after restart.",
+                    ).buttons("Restart", "Cancel").defaultButton("Restart").show(project)
+
+                    if (clickResult == "Restart") {
+                        ProjectManager.getInstance().reloadProject(project)
                     }
                 }
             }
-
-            configuration.readExternal(element)
-
-            hasModifiedSomething = true
         }
 
-        // The migration process has completed fully, the settings version can be saved
-        settings.state.settingsVersion = 2
+        // Logic for cleaning-up old MicroPython Tools libraries
+        /*delay(20000)
 
-        if (!hasModifiedSomething) {
-            return
-        }
+        DumbService.getInstance(project).smartInvokeLater {
+            ApplicationManager.getApplication().runWriteAction {
+                // Clean up library table
+                val projectLibraryTable = LibraryTablesRegistrar.getInstance().getLibraryTable(project)
+                val projectLibraryModel = projectLibraryTable.modifiableModel
 
-        Notifications.Bus.notify(
-            Notification(
-                NOTIFICATION_GROUP,
-                "MicroPython tools update",
-                "MicroPython Tools run configurations have been automatically updated.<br><br>" +
-                        "For the changes to take effect, the IDE needs to be restarted.<br><br>" +
-                        "NOTE: If your run configurations were stored in a file, you'll need to reconfigure them after restart.",
-                NotificationType.WARNING
-            ).addAction(object : NotificationAction("Restart IDE") {
-                override fun actionPerformed(e: AnActionEvent, notification: Notification) {
-                    ProjectManager.getInstance().reloadProject(project)
-                    notification.expire()
+                val librariesToRemove = projectLibraryModel.libraries.filter {
+                    it.name in listOf("MicroPythonTools", "MicroPython Tools")
                 }
-            }),
-            project
-        )
 
-        withContext(Dispatchers.EDT) {
-            val clickResult = MessageDialogBuilder.Message(
-                "MicroPython Tools Update",
-                "MicroPython Tools run configurations have been automatically updated.\n\n" +
-                        "For the changes to take effect, the IDE needs to be restarted.\n\n" +
-                        "NOTE: If your run configurations were stored in a file, you'll need to reconfigure them after restart.",
-            ).buttons("Restart", "Cancel").defaultButton("Restart").show(project)
+                librariesToRemove.forEach { library ->
+                    projectLibraryModel.removeLibrary(library)
+                }
 
-            if (clickResult == "Restart") {
-                ProjectManager.getInstance().reloadProject(project)
+                projectLibraryModel.commit()
+
+                // Clean up order entries
+                val moduleManager = ModuleManager.getInstance(project)
+                val modules = moduleManager.modules
+
+                modules.forEach { module ->
+                    val moduleRootManager = ModuleRootManager.getInstance(module)
+                    val moduleRootModel = moduleRootManager.modifiableModel
+
+                    // Find all order entries related to MicroPython libraries
+                    val entriesToRemove = moduleRootModel.orderEntries.filter { entry ->
+                        entry is LibraryOrderEntry && (entry.libraryName in listOf("MicroPythonTools", "MicroPython Tools"))
+                    }
+
+                    if (entriesToRemove.isNotEmpty()) {
+                        entriesToRemove.forEach { entry ->
+                            moduleRootModel.removeOrderEntry(entry)
+                        }
+
+                        moduleRootModel.commit()
+                    }
+                }
+
+                println("Finished cleaning up all order entries")
             }
-        }
+        }*/
     }
 }
