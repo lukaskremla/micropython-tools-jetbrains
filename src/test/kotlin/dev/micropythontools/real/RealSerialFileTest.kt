07d93803dc0e656a6b4425f05983d4800ac61117
Lukáš Kremla <155779787+lukaskremla@users.noreply.github.com>
2025-04-17 13:11:51 +0200

    Removed unused kotlin tests, for now

--- src/test/kotlin/dev/micropythontools/real/RealSerialFileTest.kt
+++
@@ -1,9 +0,0 @@
-package dev.micropythontools.real
-
-import dev.micropythontools.ui.ConnectionParameters
-import org.junit.jupiter.api.Disabled
-
-private const val PORT_NAME = "COM11"
-
-@Disabled("Works only if a real board is at $PORT_NAME")
-class RealSerialFileTest : FileTest(ConnectionParameters(PORT_NAME))
